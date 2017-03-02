31c3cf36c90f476aad5f7a926131cc6ea478745e
Aradeea <anamarlopezcontreras@gmail.com>
2017-03-02 20:23:36 +0100

    Add User devise. Index, new and show working for comics controller

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -4,22 +4,35 @@ class ComicsController < ApplicationController
     render 'index'
   end
 
-  def create
-    @comics = Comic.new
-    #Aquí hay un formulario donde se ponen las cosas para el nuevo cómic
-  end
-
   def new
+    @comics = Comic.new
     #Pilla los params para hacer un nuevo comic
   end
 
+  def create
+    @my_comic = Comic.new(
+    :title => params[:comic][:title],
+    :author => params[:comic][:author],
+    :year => params[:comic][:year],
+    :publisher => params[:comic][:publisher],
+    )
+    @my_comic.save
+    redirect_to "/comics/#{@my_comic.id}"
+  end
+
   def show
-    #Enseñame este comic concreto
+    #Enseñame este comic concreto. Despues de hacer el post
+    @selected_comic = Comic.find_by(id: params[:id])
+    render "show"
   end
 
   def edit
+
   end
 
   def destroy
+    @selected_comic = Comic.find_by(id: params[:id])
+    @selected_comic.destroy
+    redirect_to "/comics/"
   end
 end
