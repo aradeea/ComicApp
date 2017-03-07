ff43df58eef9f6ed123b6f30c63070ac956a46a3
Aradeea <anamarlopezcontreras@gmail.com>
2017-03-07 20:29:24 +0100

    Edit working, destroy not working

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,51 +1,68 @@
 class ComicsController < ApplicationController
-  def index
-    @comics = Comic.all
-    render 'index'
-  end
+    def index
+        @comics = Comic.all
+        render 'index'
+    end
 
-  def new
-    @comics = Comic.new
-    #Pilla los params para hacer un nuevo comic
-  end
+    def new
+        @comics = Comic.new
+        # Pilla los params para hacer un nuevo comic
+    end
 
-  def create
-    @my_comic = Comic.new(
-    :title => params[:comic][:title],
-    :author => params[:comic][:author],
-    :year => params[:comic][:year],
-    :publisher => params[:comic][:publisher],
-    :user_id => current_user.id,
-    :price => params[:comic][:price],
-    )
-    @my_comic.save
-    redirect_to "/comics/#{@my_comic.id}"
-  end
+    def create
+        @my_comic = Comic.new(
+            title: params[:comic][:title],
+            author: params[:comic][:author],
+            year: params[:comic][:year],
+            publisher: params[:comic][:publisher],
+            user_id: current_user.id,
+            price: params[:comic][:price]
+        )
+        @my_comic.save
+        redirect_to "/comics/#{@my_comic.id}"
+    end
 
-  def show
-    #Enseñame este comic concreto. Despues de hacer el post
-    @selected_comic = Comic.find_by(id: params[:id])
-    render "show"
-    @user = User.find_by(id: current_user.id)
-    # @comics_from_user = @User.comics.all
-  end
+    def show
+        # Enseñame este comic concreto. Despues de hacer el post
+        @comic = current_user.comics.find(params[:id])
+        @user = User.find_by(id: current_user.id)
+        render 'show'
+        # @comics_from_user = @User.comics.all
+    end
 
-  def edit #estabas por aquí!!!
-    @selected_comic = Comic.find_by(id: params[:id])
-    @my_user = User.find_by(id: @selected_comic.user_id)
-    # [:user_id]
-    # @my_comic = @my_user.@selected_comic
-    # comics.find_by(id: current_user.id)
+    def edit # TODO: estabas por aquí!!!
+        # @selected_comic = Comic.find_by(id: params[:id])
+        # @my_user = User.find_by(id: @selected_comic.user_id)
+        @comic = current_user.comics.find(params[:id])
+        # [:user_id]
+        # @my_comic = @my_user.@selected_comic
+        # comics.find_by(id: current_user.id)
 
-    # @my_user = User.find params[:user_id]
-    # @my_comic = @my_user.comic.find params[:id]
-  end
+        # @my_user = User.find params[:user_id]
+        # @my_comic = @my_user.comic.find params[:id]
+    end
 
-# def destroy
-#   @my_user = current_user.id
-#     @my_comic = @my_project.time_entries.find params[:id]
-#     @my_entry.destroy
-#     redirect_to "/projects/#{@my_project.id}/time_entries"
-# end
+    def update
+        @comic = current_user.comics.find(params[:id])
 
+        # if
+        @comic.update(comic_params)
+        redirect_to "/users/#{current_user.id}/"
+        # else
+        #   render "edit"
+        # end
+    end
+
+    def destroy
+        @user = User.find_by(id: current_user.id)
+        @comic = current_user.comics.find(params[:id])
+        @comic.destroy
+        redirect_to "/users/#{@user.id}/"
+    end
+end
+
+private # Metodo que solo se puede ejecutar dentro de la clase
+
+def comic_params
+    params.require(:comic).permit(:title, :author, :year, :publisher, :price) # Estos params tienen que tener la key time_entry (ya lo tienen, en los params de la tabla) Esto impide que se meta algun parámetro ficticio
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :comics
 
-  get "/users/:user_id", to: "users#show"
+  get "/users/:user_id", to: "users#show", as: 'user_profile'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
